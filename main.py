fb6236d4e4894f721d5914757b9ac5faad147f3f
ajaykarki <ajaykarki333@gmail.com>
2022-03-02 14:13:30 +0545

    Adding db

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from ariadne.constants import PLAYGROUND_HTML
 from flask import Flask, request, jsonify
 from ariadne import gql, QueryType, MutationType, make_executable_schema, graphql_sync
@@ -10,6 +12,7 @@ type_defs = gql(
     }
 
     type Place {
+        id: String
         name: String!
         description: String!
         country: String!
@@ -31,24 +34,43 @@ mutation = MutationType()
 # Define Resolvers
 @query.field("places")
 def places(*_):
-    return [
-       {"name": "Paris", "description": "The city of lights", "country": "France"},
-       {"name": "Rome", "description": "The city of pizza", "country": "Italy"},
-       {
-           "name": "London",
-           "description": "The city of big buildings",
-           "country": "United Kingdom",
-       },
-   ]
+   return [place.to_json() for place in Places.query.all()]
 
 @mutation.field("add_place")
 def add_place(_, info, name, description, country):
-   places.append({"name": name, "description": description, "country": country})
-   return {"name": name, "description": description, "country": country}
+   place = Places(name=name, description=description, country=country)
+   place.save()
+   return place.to_json()
 
-
+# Create executable schema
 schema = make_executable_schema(type_defs, [query, mutation])
+
+# initialize flask app
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Places(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(80), nullable=False)
+   description = db.Column(db.String(255), nullable=False)
+   country = db.Column(db.String(80), nullable=False)
+
+   def to_json(self):
+       return {
+           "id": self.id,
+           "name": self.name,
+           "description": self.description,
+           "country": self.country,
+       }
+
+   def save(self):
+       db.session.add(self)
+       db.session.commit()
 
 # Create a GraphQL Playground UI for the GraphQL schema
 @app.route("/graphql", methods=["GET"])
@@ -69,13 +91,4 @@ def graphql_server():
 
 # Run the app
 if __name__ == "__main__":
-   places = [
-       {"name": "Paris", "description": "The city of lights", "country": "France"},
-       {"name": "Rome", "description": "The city of pizza", "country": "Italy"},
-       {
-           "name": "London",
-           "description": "The city of big buildings",
-           "country": "United Kingdom",
-       },
-   ]
    app.run(debug=True)
